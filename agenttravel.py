27eae01968ea8dfa7117484e21b05aabe15b869c
gansere.coulibaly <gansere.coulibaly@gmail.com>
2025-06-16 23:32:32 +0200

    tarif enfant adulte

--- agenttravel.py
+++ agenttravel.py
@@ -4,40 +4,40 @@ import random
 def obtenir_activites_getyourguide(budget, activites_supplementaires=None):
     activites = {
         "faible": [
-            {"nom": "Promenade sur les quais de Seine", "prix": 0, "duree": 120},
-            {"nom": "Visite du Sacré-Cœur", "prix": 0, "duree": 90},
-            {"nom": "Flânerie dans le quartier Latin", "prix": 0, "duree": 120},
-            {"nom": "Exploration du Marché des Enfants Rouges", "prix": 0, "duree": 90},
-            {"nom": "Pique-nique au Jardin du Luxembourg", "prix": 0, "duree": 120},
-            {"nom": "Balade sur la Coulée verte René-Dumont", "prix": 0, "duree": 150},
-            {"nom": "Visite du Parc des Buttes-Chaumont", "prix": 0, "duree": 120},
-            {"nom": "Découverte des passages couverts", "prix": 0, "duree": 120},
-            {"nom": "Promenade au Canal Saint-Martin", "prix": 0, "duree": 120},
-            {"nom": "Balade au cimetière du Père-Lachaise", "prix": 0, "duree": 120},
+            {"nom": "Promenade sur les quais de Seine", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite du Sacré-Cœur", "prix": 0, "duree": 90, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Flânerie dans le quartier Latin", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Exploration du Marché des Enfants Rouges", "prix": 0, "duree": 90, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Pique-nique au Jardin du Luxembourg", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Balade sur la Coulée verte René-Dumont", "prix": 0, "duree": 150, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite du Parc des Buttes-Chaumont", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Découverte des passages couverts", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Promenade au Canal Saint-Martin", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Balade au cimetière du Père-Lachaise", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
         ],
         "moyen": [
-            {"nom": "Billet coupe-file Musée du Louvre", "prix": 17, "duree": 180},
-            {"nom": "Tour Eiffel : billet sommet", "prix": 26, "duree": 120},
-            {"nom": "Croisière sur la Seine", "prix": 15, "duree": 60},
-            {"nom": "Visite guidée du Musée d'Orsay", "prix": 16, "duree": 120},
-            {"nom": "Atelier de fabrication de macarons", "prix": 45, "duree": 150},
-            {"nom": "Visite des Catacombes de Paris", "prix": 29, "duree": 90},
-            {"nom": "Dégustation de vins et fromages", "prix": 55, "duree": 120},
-            {"nom": "Visite du Palais Garnier", "prix": 14, "duree": 90},
-            {"nom": "Balade à vélo dans Paris", "prix": 30, "duree": 180},
-            {"nom": "Visite du Musée Rodin", "prix": 12, "duree": 120},
+            {"nom": "Billet coupe-file Musée du Louvre", "prix": 17, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Tour Eiffel : billet sommet", "prix": 26, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Croisière sur la Seine", "prix": 15, "duree": 60, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite guidée du Musée d'Orsay", "prix": 16, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Atelier de fabrication de macarons", "prix": 45, "duree": 150, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite des Catacombes de Paris", "prix": 29, "duree": 90, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dégustation de vins et fromages", "prix": 55, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite du Palais Garnier", "prix": 14, "duree": 90, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Balade à vélo dans Paris", "prix": 30, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite du Musée Rodin", "prix": 12, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
         ],
         "élevé": [
-            {"nom": "Visite guidée privée du Louvre", "prix": 250, "duree": 180},
-            {"nom": "Dîner-croisière de luxe", "prix": 150, "duree": 180},
-            {"nom": "Spectacle Moulin Rouge (avec champagne)", "prix": 120, "duree": 120},
-            {"nom": "Cours de cuisine gastronomique", "prix": 180, "duree": 180},
-            {"nom": "Excursion en montgolfière au-dessus de Paris", "prix": 300, "duree": 120},
-            {"nom": "Séance photo privée à Paris", "prix": 200, "duree": 120},
-            {"nom": "Visite en hélicoptère de Paris", "prix": 400, "duree": 60},
-            {"nom": "Dégustation de champagne dans une cave", "prix": 100, "duree": 120},
-            {"nom": "Shopping privé dans les boutiques de luxe", "prix": 200, "duree": 180},
-            {"nom": "Visite du Château de Versailles avec guide privé", "prix": 280, "duree": 240},
+            {"nom": "Visite guidée privée du Louvre", "prix": 250, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dîner-croisière de luxe", "prix": 150, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Spectacle Moulin Rouge (avec champagne)", "prix": 120, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Cours de cuisine gastronomique", "prix": 180, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Excursion en montgolfière au-dessus de Paris", "prix": 300, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Séance photo privée à Paris", "prix": 200, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite en hélicoptère de Paris", "prix": 400, "duree": 60, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dégustation de champagne dans une cave", "prix": 100, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Shopping privé dans les boutiques de luxe", "prix": 200, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite du Château de Versailles avec guide privé", "prix": 280, "duree": 240, "tarifAdulte":0, "tarifEnfant":0},
         ],
     }
  
@@ -91,40 +91,40 @@ def obtenir_restaurants(budget):
 def obtenir_activites_nocturnes(budget):
     activites_nocturnes = {
         "faible": [
-            {"nom": "Promenade nocturne sur les Champs-Élysées", "prix": 0, "duree": 120},
-            {"nom": "Concert dans un bar de jazz", "prix": 10, "duree": 120},
-            {"nom": "Cinéma en plein air", "prix": 12, "duree": 120},
-            {"nom": "Soirée dans un bar étudiant", "prix": 8, "duree": 180},
-            {"nom": "Balade le long du Canal Saint-Martin", "prix": 0, "duree": 120},
-            {"nom": "Visite nocturne du Quartier Latin", "prix": 0, "duree": 120},
-            {"nom": "Dégustation de bières artisanales", "prix": 15, "duree": 120},
-            {"nom": "Spectacle de rue", "prix": 0, "duree": 60},
-            {"nom": "Soirée jeux de société dans un café", "prix": 5, "duree": 120},
-            {"nom": "Karaoké dans un bar", "prix": 7, "duree": 180},
+            {"nom": "Promenade nocturne sur les Champs-Élysées", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Concert dans un bar de jazz", "prix": 10, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Cinéma en plein air", "prix": 12, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée dans un bar étudiant", "prix": 8, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Balade le long du Canal Saint-Martin", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite nocturne du Quartier Latin", "prix": 0, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dégustation de bières artisanales", "prix": 15, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Spectacle de rue", "prix": 0, "duree": 60, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée jeux de société dans un café", "prix": 5, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Karaoké dans un bar", "prix": 7, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
         ],
         "moyen": [
-            {"nom": "Spectacle de cabaret", "prix": 50, "duree": 120},
-            {"nom": "Dégustation de vins", "prix": 40, "duree": 120},
-            {"nom": "Visite nocturne d'un musée", "prix": 25, "duree": 120},
-            {"nom": "Dîner-spectacle", "prix": 80, "duree": 180},
-            {"nom": "Soirée dans un club de jazz", "prix": 35, "duree": 120},
-            {"nom": "Balade en bateau-mouche", "prix": 20, "duree": 60},
-            {"nom": "Visite guidée nocturne de Paris", "prix": 30, "duree": 120},
-            {"nom": "Soirée dans un bar à cocktails", "prix": 45, "duree": 120},
-            {"nom": "Concert dans une salle de spectacle", "prix": 60, "duree": 120},
-            {"nom": "Match de football au Parc des Princes", "prix": 70, "duree": 120},
+            {"nom": "Spectacle de cabaret", "prix": 50, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dégustation de vins", "prix": 40, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite nocturne d'un musée", "prix": 25, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dîner-spectacle", "prix": 80, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée dans un club de jazz", "prix": 35, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Balade en bateau-mouche", "prix": 20, "duree": 60, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Visite guidée nocturne de Paris", "prix": 30, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée dans un bar à cocktails", "prix": 45, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Concert dans une salle de spectacle", "prix": 60, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Match de football au Parc des Princes", "prix": 70, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
         ],
         "élevé": [
-            {"nom": "Opéra à l'Opéra Garnier", "prix": 150, "duree": 180},
-            {"nom": "Dîner gastronomique avec vue", "prix": 200, "duree": 180},
-            {"nom": "Soirée privée dans un club exclusif", "prix": 300, "duree": 180},
-            {"nom": "Spectacle au Lido", "prix": 180, "duree": 120},
-            {"nom": "Soirée dans un bar à champagne", "prix": 120, "duree": 120},
-            {"nom": "Dégustation de grands crus", "prix": 250, "duree": 120},
-            {"nom": "Ballet à l'Opéra Bastille", "prix": 160, "duree": 120},
-            {"nom": "Soirée dans un club de jazz privé", "prix": 220, "duree": 120},
-            {"nom": "Concert dans une salle de spectacle prestigieuse", "prix": 280, "duree": 120},
-            {"nom": "Match de football VIP au Parc des Princes", "prix": 350, "duree": 120},
+            {"nom": "Opéra à l'Opéra Garnier", "prix": 150, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dîner gastronomique avec vue", "prix": 200, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée privée dans un club exclusif", "prix": 300, "duree": 180, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Spectacle au Lido", "prix": 180, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée dans un bar à champagne", "prix": 120, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Dégustation de grands crus", "prix": 250, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Ballet à l'Opéra Bastille", "prix": 160, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Soirée dans un club de jazz privé", "prix": 220, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Concert dans une salle de spectacle prestigieuse", "prix": 280, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
+            {"nom": "Match de football VIP au Parc des Princes", "prix": 350, "duree": 120, "tarifAdulte":0, "tarifEnfant":0},
         ],
     }
     return activites_nocturnes[budget]
@@ -231,15 +231,23 @@ def afficher_itineraire(itineraires, lieu_depart=None):
         print(f"Jour {jour['jour']}:")
         if lieu_depart != "": 
             print(f"Départ {lieu_depart}:")
-        print(f"  9h00-12h00 : {', '.join([a['nom'] + f' (Prix : {a['prix']}€, Durée : {a['duree']} minutes)' for a in jour['matin']])}")
-        print(f"  12h00-13h30 : {jour['midi']['nom']} ({jour['midi']['type']})")
-        print(f"  13h30-19h00 : {', '.join([a['nom'] + f' (Prix : {a['prix']}€, Durée : {a['duree']} minutes)' for a in jour['apres_midi']])}")
-        print(f"  19h30-23h00 : {jour['soir']['nom']} (Prix : {jour['soir']['prix']}€, Durée : {jour['soir']['duree']} minutes)")
-        print(f"  Prix total de la journée (hors repas) : {prix_total_jour}€")
+        print(f"9h00-12h00 : {', '.join([a['nom'] + f' Durée : {a['duree']} minutes \n'
+             +f'  Prix : {a['prix']}€\n'  
+             +f'  Prix Adulte: {a['tarifAdulte']}€\nPrix Enfant : {a['tarifEnfant']}€\n ' for a in jour['matin']])}")
+        
+        print(f"12h00-13h30 : {jour['midi']['nom']} ({jour['midi']['type']})")
+        
+                
+        print(f"13h30-19h00 : {''.join([a['nom'] + f' Durée : {a['duree']} minutes \n'
+             +f'  Prix : {a['prix']}€\n'  
+             +f'  Prix Adulte: {a['tarifAdulte']}€\n  Prix Enfant : {a['tarifEnfant']}€\n\n' for a in jour['apres_midi']])}")
+        
+        print(f"19h30-23h00 : {jour['soir']['nom']} (Prix : {jour['soir']['prix']}€  Durée : {jour['soir']['duree']} minutes)\n")
+        print(f"Prix total de la journée (hors repas) : {prix_total_jour}€")
         if minutes == 0:
-            print(f"  Durée totale des activités : {heures} heures")
+            print(f"Durée totale des activités : {heures} heures")
         else:
-            print(f"  Durée totale des activités : {heures} heures et {minutes} minutes")
+            print(f"Durée totale des activités : {heures} heures et {minutes} minutes")
             
         print("-" * 20)        
 
